4449eb80e532494aa943997556706246718cbc55
saifulislammunna <saifulmunna28@gmail.com>
2021-10-13 21:52:43 +0600

    unsubscribe-added

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -25,13 +25,13 @@ const useFirebase = () => {
     // observe whether user auth state changed or not
 
     useEffect( () => {
-        onAuthStateChanged(auth, (user) => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
               setUser(user)
-               
-               
-            }  
-          });
+                }  
+             });
+
+             return unsubscribe;
     },[])
 
     return  {
