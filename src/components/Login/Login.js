098113c5179c92601f68f3bc8352243376d0af1f
saifulislammunna <saifulmunna28@gmail.com>
2021-10-14 00:09:59 +0600

    added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -29,7 +29,7 @@ const Login = () => {
                     <input type="submit" value="Submit" />
 
                 </form>
-                <p>new to ema-john? <Link to="/register">Create Account</Link></p>
+                <p>new to ema-john website? <Link to="/register">Create Account</Link></p>
                 <div>----------or------------</div>
                 <button 
                 className="btn-regular"
